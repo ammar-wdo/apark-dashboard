7e1e4e44cf3e70af48c70d0a4516083cf19d3f33
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-11-15 11:17:20 +0400

    push

--- app/(dashboard)/dashboard/services/page.tsx
+++ app/(dashboard)/dashboard/services/page.tsx
@@ -42,7 +42,7 @@ const page = async ({  }: Props) => {
         <div>no services added</div>
       ) : (
         <div className={"grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  3xl:grid-cols-5 gap-5"}>
-          {company.services.map(service=><Suspense key={service.id} fallback={<ServiceCardSceleton />}><ServiceCard key={service.id} serviceId={service.id} companyId={company.id} /></Suspense>)}
+          {company.services.map(service=><ServiceCard key={service.id} serviceId={service.id} companyId={company.id} />)}
         </div>
       )}
 
