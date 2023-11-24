e0da116ec2bcf4d4e9489e877f4e1c652331b65a
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-11-24 07:46:50 +0400

    payment

--- app/api/(public)/webhook/route.ts
+++ app/api/(public)/webhook/route.ts
@@ -26,23 +26,28 @@ export async function POST(req: Request) {
 
   console.log(session.metadata);
 
-  switch(event.type){
+  switch (event.type) {
+    case "checkout.session.completed": {
+      try {
+        if (session.payment_status === "paid") {
+          const order = await prisma.booking.update({
+            where: {
+              id: session?.metadata?.id,
+            },
+            data: {
+              paymentStatus: "SUCCEEDED",
+            },
+          });
+          // await sendMail('Booking is payed',"new booking is payed","m.swaghi@gmail.com","Mouhammmad")
+        }
+      } catch (error) {
+        console.log(error);
+      }
 
-    case "checkout.session.completed": try {
-      const order = await prisma.booking.update({
-        where: {
-          id: session?.metadata?.id,
-        },
-        data: {
-          paymentStatus: "SUCCEEDED",
-        },
-      });
-      // await sendMail('Booking is payed',"new booking is payed","m.swaghi@gmail.com","Mouhammmad")
-    } catch (error) {
-      console.log(error);
-    };
+      break;
+    }
 
-    case "checkout.session.expired":
+    case "checkout.session.expired": {
       try {
         const order = await prisma.booking.update({
           where: {
@@ -54,35 +59,29 @@ export async function POST(req: Request) {
         });
       } catch (error) {
         console.log(error);
-      };
+      }
+      break;
+    }
 
-      case "payment_intent.canceled" :
-        try {
-          const order = await prisma.booking.update({
-            where: {
-              id: session?.metadata?.id,
-            },
-            data: {
-              paymentStatus: "EXPIRED",
-            },
-          });
-        } catch (error) {
-          console.log(error);
-        };
-  };
+    case "checkout.session.async_payment_failed": {
+      try {
+        const order = await prisma.booking.update({
+          where: {
+            id: session?.metadata?.id,
+          },
+          data: {
+            paymentStatus: "FAILED",
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+      break;
+    }
 
-
-  
-
-
-  
-     
-    
-
-    
- 
-
-  
+    default:
+      console.log("default");
+  }
 
   return new NextResponse(null, { status: 200 });
 }
