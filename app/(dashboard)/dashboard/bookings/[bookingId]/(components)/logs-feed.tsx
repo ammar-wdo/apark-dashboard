1d5cf8355ee75565cd9657d4836cdb5e06019370
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-11-29 10:33:08 +0400

    to cancel

--- app/(dashboard)/dashboard/bookings/[bookingId]/(components)/logs-feed.tsx
+++ app/(dashboard)/dashboard/bookings/[bookingId]/(components)/logs-feed.tsx
@@ -39,6 +39,7 @@ const themes:{[key:string]:string} = {CANCELED:'text-rose-500 bg-rose-500/20',EX
       <TableHead >Booking status</TableHead>
       <TableHead>Payment status</TableHead>
       <TableHead>Payed</TableHead>
+      <TableHead>Created At</TableHead>
 
     </TableRow>
   </TableHeader>
@@ -49,6 +50,7 @@ const themes:{[key:string]:string} = {CANCELED:'text-rose-500 bg-rose-500/20',EX
       <TableCell ><span className={cn('font-semibold rounded-md  p-3 py-2 text-xs ',themes[log.bookingStatus])}>{log.bookingStatus}</span></TableCell>
       <TableCell ><span className={cn('font-semibold rounded-md  p-3 py-2 text-xs ',themes[log.paymentStatus!])}>{log.paymentStatus}</span></TableCell>
       <TableCell>${log.payed}</TableCell>
+      <TableCell>{log.createdAt.toLocaleString()}</TableCell>
  
     </TableRow>)}
   
