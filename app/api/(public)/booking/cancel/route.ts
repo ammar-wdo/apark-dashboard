711333393596079d1cc4c15e18a0cb868d43a1b2
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-11-30 13:40:00 +0400

    cancel

--- app/api/(public)/booking/cancel/route.ts
+++ app/api/(public)/booking/cancel/route.ts
@@ -104,7 +104,7 @@ export async function POST(req: Request) {
       const values = setLog(
         0,
         "CANCELED",
-        "This booking is requiring a refund action ",
+        "This booking has been canceled and requiring a refund action ",
         updatedBooking
       );
       const newLog = prisma.log.create({
