19e140cdc508b046d23ba2a2d8c638dacadd88a1
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-12-04 13:19:42 +0400

    booking dashboard

--- app/(dashboard)/dashboard/(components)/boxes/(helpers)/get-total.ts
+++ app/(dashboard)/dashboard/(components)/boxes/(helpers)/get-total.ts
@@ -1,39 +1,44 @@
 import { Booking, Service } from "@prisma/client";
 
-type Services = Service & {bookings:Booking[]}
+type Services = Service & { bookings: Booking[] };
 
-export const getTotal = (services : Services | Services[])=>{
-    if (services) {
-        let totalRevenue = 0;
-        let monthlyRevenue = 0;
-        const currentDate = new Date();
-        const currentMonth = currentDate.getMonth();
-    
-        if (Array.isArray(services)) {
-          // Calculate revenue from all services
-          services.forEach(service => {
-            service.bookings.forEach(booking => {
-              totalRevenue += booking.total;
-              const bookingMonth = new Date(booking.createdAt).getMonth();
-              if (bookingMonth === currentMonth) {
-                monthlyRevenue += booking.total;
-              }
-            });
-          });
-        } else {
-          // Calculate revenue from a single service
-          services.bookings.forEach(booking => {
-            totalRevenue += booking.total;
-            const bookingMonth = new Date(booking.createdAt).getMonth();
-            if (bookingMonth === currentMonth) {
-              monthlyRevenue += booking.total;
-            }
-          });
+export const getTotal = (services: Services | Services[]) => {
+  if (services) {
+    let totalRevenue = 0;
+    let monthlyRevenue = 0;
+    let totalBookings = 0;
+    let monthlyBookings = 0;
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth();
+  
+    if (Array.isArray(services)) {
+      // Calculate revenue and bookings from all services
+      services.forEach((service) => {
+        service.bookings.forEach((booking) => {
+          totalRevenue += booking.total;
+          totalBookings++;
+          const bookingMonth = new Date(booking.createdAt).getMonth();
+          if (bookingMonth === currentMonth) {
+            monthlyRevenue += booking.total;
+            monthlyBookings++;
+          }
+        });
+      });
+    } else {
+      // Calculate revenue and bookings from a single service
+      services.bookings.forEach((booking) => {
+        totalRevenue += booking.total;
+        totalBookings++;
+        const bookingMonth = new Date(booking.createdAt).getMonth();
+        if (bookingMonth === currentMonth) {
+          monthlyRevenue += booking.total;
+          monthlyBookings++;
         }
-        
-        return { totalRevenue, monthlyRevenue };
-      }
-      
-      return { totalRevenue: 0, monthlyRevenue: 0 };
-
-}
+      });
+    }
+    
+    return { totalRevenue, monthlyRevenue, totalBookings, monthlyBookings };
+  }
+  
+  return { totalRevenue: 0, monthlyRevenue: 0, totalBookings: 0, monthlyBookings: 0 };
+};
