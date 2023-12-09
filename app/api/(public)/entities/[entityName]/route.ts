b937725b43052efe886003ac55418c4a2095e224
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-12-09 08:52:42 +0400

    UI

---
+++ app/api/(public)/entities/[entityName]/route.ts
@@ -0,0 +1,35 @@
+import prisma from "@/lib/db";
+import { NextResponse } from "next/server";
+
+
+export async function GET(req:Request,{params}:{params:{entityName:string}}){
+console.log('done')
+
+    try {
+const entityName = params.entityName
+console.log("entityName",entityName)
+
+if(!entityName) return NextResponse.json({error:'entity name is required'},{status:400})
+
+
+const entity =await  prisma.entity.findFirst({
+    where:{
+        entityName
+    },
+    include:{
+        airport:{
+            select:{name:true}
+        }
+    }
+})
+console.log(entity)
+return NextResponse.json({entity},{status:200})
+        
+        
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({error:"internal error"},{status:500})
+    }
+
+
+}
