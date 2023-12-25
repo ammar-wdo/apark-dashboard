4cf7d98addbebb340b49e0fd743ff738bb0a6c54
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-12-25 14:26:21 +0400

    time zone

---
+++ app/(dashboard)/dashboard/services/[serviceId]/extra-options/loading.tsx
@@ -0,0 +1,22 @@
+import { Skeleton } from '@/components/ui/skeleton'
+import React from 'react'
+
+type Props = {}
+
+const loading = (props: Props) => {
+  return (
+    <div>
+         <div className='space-y-2 mb-6'>
+        <Skeleton className='text-3xl first-letter:uppercase font-bold w-[100px] p-2'/>
+        <Skeleton className='text-sm text-muted-foreground p-2 w-[200px]'/>
+    </div>
+
+    <div className='w- full flex items-center gap-4 mt-10 flex-wrap'>
+        {Array(10).fill('').map((_,i)=><Skeleton className='w-[375px] h-[475px]' />)}
+
+    </div>
+    </div>
+  )
+}
+
+export default loading
