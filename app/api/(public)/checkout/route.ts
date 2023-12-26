81b331000131a779dac6ef23f2c8dfa0b3cd8f35
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-12-26 14:14:49 +0400

    fix price

--- app/api/(public)/checkout/route.ts
+++ app/api/(public)/checkout/route.ts
@@ -200,7 +200,7 @@ export async function POST(req: Request) {
                   : ""
               }`,
             },
-            unit_amount: +booking.total.toFixed(2)  * 100 ,
+            unit_amount: Math.round(booking.total * 100),
           },
           quantity: 1,
         },
