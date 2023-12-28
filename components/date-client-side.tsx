8bdd33622fc57564467605e7399055928ced46da
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-12-28 14:22:11 +0400

    date show

--- components/date-client-side.tsx
+++ components/date-client-side.tsx
@@ -8,6 +8,7 @@ type Props = {
 const DateClientSide = ({theDate}: Props) => {
 
     const formattedDate = format(new Date(theDate), "EEE, MMM/d, HH:mm");
+    console.log(formattedDate)
   return (
     <p className="text-xs text-neutral-500 pt-3 absolute bottom-2 right-3 dark:text-neutral-200">
         {formattedDate}
