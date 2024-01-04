8063a9a2e30f4f0fcacc92fcfef0a15f32c9aa03
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2024-01-04 07:59:00 +0400

    to zuplo

--- app/(dashboard)/dashboard/(components)/boxes/daily-chart.tsx
+++ app/(dashboard)/dashboard/(components)/boxes/daily-chart.tsx
@@ -13,6 +13,8 @@ const DailyChart = ({data,bookingsPerDay}: Props) => {
 
     const usedDate =Array.isArray(data) ? data.map(el=>({name:el.day,total:el.revenue})) : data
     const tickCount = bookingsPerDay ? Math.max(...bookingsPerDay.map(d => d.total)) + 1 : undefined;
+
+   
   return (
     <ResponsiveContainer  height={500}>
     <BarChart
@@ -32,7 +34,7 @@ const DailyChart = ({data,bookingsPerDay}: Props) => {
       <Tooltip />
       
    
-      <Bar dataKey="total" fill="#6366F1"  activeBar={<Rectangle fill="gold" stroke="purple" />}  maxBarSize={35}/>
+      <Bar dataKey="total" fill="#6366F1"  activeBar={<Rectangle fill="gold" stroke="purple" radius={3}  />} radius={3}  maxBarSize={25} />
     </BarChart>
   </ResponsiveContainer>
   )
