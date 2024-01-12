df03528249dd73ea7ce74bfa39571687cb12c00a
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2024-01-12 13:50:55 +0400

    refine

--- app/api/(public)/booking/cancel/(helper)/send-email.ts
+++ app/api/(public)/booking/cancel/(helper)/send-email.ts
@@ -10,6 +10,7 @@ export const sendEmail = async (
   const object = {
     id: booking.id,
     service: name,
+    email:booking.email,
     bookingCode: booking.bookingCode,
     arrivalString: `${booking.arrivalDate.getDate()}-${
       booking.arrivalDate.getMonth() + 1
@@ -21,72 +22,61 @@ export const sendEmail = async (
     lastName: booking.lastName,
   };
 
-
-    if (type === "refund") {
-
-        try {
-            await axios.post(
-                "https://webhooks.integrately.com/a/webhooks/db2d6b294808450ca975ebe2fd02b699",
-                object
-              );
-        } catch (error) {
-                   await prisma.notification.create({
-                data: {
-                  IdHolder: booking.id,
-                isAdmin:true,
-                  status: "DELETE",
-                  type: "BOOKING",
-                  message:
-                    `An error happened, the user with booking code ${booking.bookingCode} did not recieve a cancelation with refund message and should be sent maually to this email ${booking.email}`,
-                },
-              });
-            console.log(error)
-        }
-    
+  if (type === "refund") {
+    try {
+      await axios.post(
+        "https://webhooks.integrately.com/a/webhooks/db2d6b294808450ca975ebe2fd02b699",
+        object
+      );
+    } catch (error) {
+      await prisma.notification.create({
+        data: {
+          IdHolder: booking.id,
+          isAdmin: true,
+          status: "DELETE",
+          type: "BOOKING",
+          message: `An error happened, the user with booking code ${booking.bookingCode} did not recieve a cancelation with refund message and should be sent maually to this email ${booking.email}`,
+        },
+      });
+      console.log(error);
     }
-    if (type === "no-refund") {
-        try {
-            await axios.post(
-                "https://webhooks.integrately.com/a/webhooks/b6f55982a9ed4f75902dd68038479036",
-                object
-              );
-        } catch (error) {
-            await prisma.notification.create({
-                data: {
-                  IdHolder: booking.id,
-                isAdmin:true,
-                  status: "DELETE",
-                  type: "BOOKING",
-                  message:
-                    `An error happened, the user with booking code ${booking.bookingCode} did not recieve a cancelation with NO refund message and should be sent maually to this email ${booking.email}`,
-                },
-              });
-            console.log(error)
-        }
-   
+  }
+  if (type === "no-refund") {
+    try {
+      await axios.post(
+        "https://webhooks.integrately.com/a/webhooks/b6f55982a9ed4f75902dd68038479036",
+        object
+      );
+    } catch (error) {
+      await prisma.notification.create({
+        data: {
+          IdHolder: booking.id,
+          isAdmin: true,
+          status: "DELETE",
+          type: "BOOKING",
+          message: `An error happened, the user with booking code ${booking.bookingCode} did not recieve a cancelation with NO refund message and should be sent maually to this email ${booking.email}`,
+        },
+      });
+      console.log(error);
     }
-    if (type === "update") {
-        try {
-            await axios.post(
-                "https://webhooks.integrately.com/a/webhooks/9df543865a9144498ddadaf80c78f15a",
-                object
-              );
-        } catch (error) {
-            await prisma.notification.create({
-                data: {
-                  IdHolder: booking.id,
-                isAdmin:true,
-                  status: "DELETE",
-                  type: "BOOKING",
-                  message:
-                    `An error happened, the user with booking code ${booking.bookingCode} did not recieve an update message and should be sent maually to this email ${booking.email}`,
-                },
-              });
-            console.log(error)
-        }
-    
+  }
+  if (type === "update") {
+    try {
+      await axios.post(
+        "https://webhooks.integrately.com/a/webhooks/9df543865a9144498ddadaf80c78f15a",
+        object
+      );
+    } catch (error) {
+      await prisma.notification.create({
+        data: {
+          IdHolder: booking.id,
+          isAdmin: true,
+          status: "DELETE",
+          type: "BOOKING",
+          message: `An error happened, the user with booking code ${booking.bookingCode} did not recieve an update message and should be sent maually to this email ${booking.email}`,
+        },
+      });
+      console.log(error);
     }
-
-
-  
+  }
 };
