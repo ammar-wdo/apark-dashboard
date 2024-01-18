7903faf57c68da4228601e6876070620368dac58
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2024-01-18 10:31:52 +0400

    auth error

--- app/(dashboard)/dashboard/(components)/error-holder.tsx
+++ app/(dashboard)/dashboard/(components)/error-holder.tsx
@@ -7,7 +7,7 @@ const ErrorHolder = (props: Props) => {
   
     return (
       <div className="flex items-center justify-center h-screen w-full">
-        <p className="text-center text-rose-500 text-2xl font-semibold">
+        <p className="text-rose-400 text-4xl font-bold text-center mb-24">
           This account is not valid!
           <br />
           Please contact Aparking admin
