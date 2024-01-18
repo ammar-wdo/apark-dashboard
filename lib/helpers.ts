c8486f4cb760ec87f88dd60ec4e7f6d8be1d2763
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2024-01-18 10:19:19 +0400

    auth error

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -11,7 +11,7 @@ import { Company, Entity } from "@prisma/client"
 export const getCurrentCompany = async() =>{
 
     const session = await getServerSession(authOptions)
-    if(!session?.user?.email || !session) throw new Error('auth')
+    if(!session?.user?.email) return null 
 
     if(session?.user?.name === "Company"){
         const company = await prisma.company.findUnique({
