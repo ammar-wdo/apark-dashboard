0e3ad3442b1f7aeac5dc65377a02a86dc1807407
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2024-01-19 07:32:32 +0400

    endpoint

--- app/api/(public)/manage/price-update/route.ts
+++ app/api/(public)/manage/price-update/route.ts
@@ -1,31 +1,51 @@
+import prisma from "@/lib/db";
 import { NextResponse } from "next/server";
 import { z } from "zod";
 
-
 const bodySchema = z.object({
-  
-    prices:z.array(z.coerce.number())
-})
+  id: z.string().min(1),
+  prices: z.array(z.coerce.number()),
+});
 
-export const POST = async (req:Request) =>{
-    try {
+export const POST = async (req: Request) => {
+  try {
+    const apiKey = req.headers.get("x-api-key");
 
-        const apiKey = req.headers.get('x-api-key');
-
-        // Verify API key
-        if (apiKey !== "my key") {
-            return NextResponse.json({ error: 'Unauthorized access' }, { status: 401 });
-        }
-
-        const body = await req.json()
-
-       const validBody = bodySchema.parse(body)
-
-
-        return NextResponse.json({prices:validBody.prices},{status:201})
-        
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({error:'Internal error'},{status:500})
+    // Verify API key
+    if (apiKey !== "my key") {
+      return NextResponse.json(
+        { error: "Unauthorized access" },
+        { status: 401 }
+      );
     }
-}
+
+    const body = await req.json();
+
+    const validBody = bodySchema.safeParse(body);
+    if (!validBody.success)
+      return NextResponse.json({ error: validBody.error }, { status: 400 });
+
+    const serviceId = validBody.data.id;
+
+    const service = await prisma.service.findUnique({
+      where: { id: serviceId },
+    });
+    if (!service)
+      return NextResponse.json(
+        { error: "service does not exist" },
+        { status: 400 }
+      );
+
+    await prisma.service.update({
+      where: { id: serviceId },
+      data: { pricings: validBody.data.prices },
+    });
+
+
+return NextResponse.json(null,{status:201})
+
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: "Internal error" }, { status: 500 });
+  }
+};
