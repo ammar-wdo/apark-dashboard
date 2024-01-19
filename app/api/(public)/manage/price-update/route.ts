f82b071ebdd16b834004ef900f2d9d2168f15469
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2024-01-19 06:55:01 +0400

    endpoint

--- app/api/(public)/manage/price-update/route.ts
+++ app/api/(public)/manage/price-update/route.ts
@@ -3,19 +3,26 @@ import { z } from "zod";
 
 
 const bodySchema = z.object({
-    key:z.literal('the-secret-key'),
+  
     prices:z.array(z.coerce.number())
 })
 
 export const POST = async (req:Request) =>{
     try {
 
+        const apiKey = req.headers.get('x-api-key');
+
+        // Verify API key
+        if (apiKey !== "my key") {
+            return NextResponse.json({ error: 'Unauthorized access' }, { status: 401 });
+        }
+
         const body = await req.json()
 
        const validBody = bodySchema.parse(body)
 
 
-        return NextResponse.json({key:validBody.key,prices:validBody.prices},{status:201})
+        return NextResponse.json({prices:validBody.prices},{status:201})
         
     } catch (error) {
         console.log(error)
