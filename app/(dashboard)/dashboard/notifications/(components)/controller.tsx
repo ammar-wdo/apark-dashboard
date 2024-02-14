df651fdaf371a0e0adaa7970bda986960cec6b3f
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2024-02-14 11:36:35 +0400

    scroller

--- app/(dashboard)/dashboard/notifications/(components)/controller.tsx
+++ app/(dashboard)/dashboard/notifications/(components)/controller.tsx
@@ -12,7 +12,7 @@ const Controller = ({list}: Props) => {
 
 
     const handleClick = ()=>{
-        router.push(`/dashboard/notifications?list=${+list +1}`)
+        router.push(`/dashboard/notifications?list=${+list +1}`,{scroll:false})
     }
   return (
     <div onClick={handleClick} className=' py-2 text-blue-500 underline cursor-pointer w-fit text-xs mx-auto'>See more</div>
--- app/(dashboard)/dashboard/notifications/(components)/scroller.tsx
+++ app/(dashboard)/dashboard/notifications/(components)/scroller.tsx
@@ -1,16 +1,18 @@
 'use client'
 
+import { useSearchParams } from 'next/navigation'
 import React, { useEffect, useRef } from 'react'
 
 type Props = {}
 
 const Scroller = (props: Props) => {
 
-
+const searchparams = useSearchParams()
     const scrollerRef = useRef<HTMLDivElement | null>(null)
 
     
     useEffect(()=>{
+      if(!searchparams.get('list')) return
         scrollerRef.current?.scrollIntoView({behavior:'smooth'})
     },[])
   return (
