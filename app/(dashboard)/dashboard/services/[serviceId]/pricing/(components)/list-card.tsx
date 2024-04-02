2faa73a4efaca0e3561e1d8be60ad2ce0636bcf8
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2024-04-02 11:16:00 +0400

    refine

--- app/(dashboard)/dashboard/services/[serviceId]/pricing/(components)/list-card.tsx
+++ app/(dashboard)/dashboard/services/[serviceId]/pricing/(components)/list-card.tsx
@@ -3,7 +3,7 @@
 import { deleteList } from '@/actions/list-actions'
 import { Button } from '@/components/ui/button'
 import { useModal } from '@/hooks/use-modal'
-import { formatDate } from '@/lib/utils'
+import { convertDateToISOString, formatDate } from '@/lib/utils'
 import { List } from '@prisma/client'
 import { Delete, Edit } from 'lucide-react'
 import Link from 'next/link'
@@ -13,11 +13,13 @@ type Props = {list:List,serviceId:string}
 
 const ListCard = ({list,serviceId}: Props) => {
     const {setOpen} = useModal()
+
+    console.log('start date',list.startDate)
   return (
     <div key={list.id} className="border p-8 rounded-md bg-white hover:shadow-lg transition">
     <p className="text-muted-foreground text-xl capitalize font-bold">{list.label || "No Label"}</p>
-    <p>Start Date: {formatDate(list.startDate)}</p>
-    <p>End Date: {formatDate(list.endDate)}</p>
+    <p>Start Date: {convertDateToISOString(list.startDate)}</p>
+    <p>End Date: {convertDateToISOString(list.endDate)}</p>
     <div className="mt-4 flex items-center gap-3">
       <Button asChild><Link href={`/dashboard/services/${serviceId}/pricing/${list.id}`} className="flex items-center gap-3 "><Edit size={20}/>Edit</Link></Button>
       <Button onClick={()=>setOpen('delete-modal',{deleteFn:()=>deleteList(serviceId,list.id)})} variant={'destructive'}><Delete className='mr-3' /> Delete</Button>
